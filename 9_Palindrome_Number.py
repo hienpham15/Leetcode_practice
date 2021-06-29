a8b13ce3e77c6ea75a746c56c85bba5c98690358
hienpham15 <phamxuanhien15@gmail.com>
2021-06-29 13:38:28 +0200

    Add second solution: revert the second of the number and compare with the first half

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -8,16 +8,39 @@ Created on Tue Jun 29 13:16:12 2021
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if x < 0:
-            return 'false'
+            return False
         elif x == 0:
-            return 'true'
+            return True
         elif x > 0:
             x_str = str(x)
             if x_str == x_str[::-1]:
-                return 'true'
+                return True
             else:
-                return 'false'
+                return False
             
+            
+    def isPalindrome_v2(self, x: int) -> bool:
+        
+        if x < 0 or (x%10 == 0 and x !=0):
+            return False
+        elif x == 0:
+            return True
+        
+        revert_num = 0
+        
+        while x > revert_num:
+            digit = x % 10
+            x = x//10
+            
+            revert_num = revert_num*10 + digit 
+        
+        if x == revert_num or x == revert_num//10:
+            return True
+        else:
+            return False
+            
+        
+    
             
 x = -121
 ans = Solution().isPalindrome(x)
