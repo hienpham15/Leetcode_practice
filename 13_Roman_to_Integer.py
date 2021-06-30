60782598a034ead9426a77d4f88657c3f3c48702
hienpham15 <phamxuanhien15@gmail.com>
2021-06-30 13:52:07 +0200

    Add second solution

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -23,6 +23,25 @@ class Solution:
                 i += 1
         return num
     
+    
+    def romanToInt_v2(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
+    
 s = 'MCMXCIV'
 ans = Solution().romanToInt(s)
             
