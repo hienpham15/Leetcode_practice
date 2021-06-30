fa32972b1815b69629c4cdc93d15da87e146f163
hienpham15 <phamxuanhien15@gmail.com>
2021-06-30 19:13:30 +0200

    Add faster solution, split array into negatives, zeros and positives

--- 15_3Sum.py
+++ 15_3Sum.py
@@ -22,6 +22,48 @@ class Solution:
                 else:
                     hmap[nums[j]] = j
         return ans
+    
+    
+    def threeSum_v2(self, nums):
+        #split into negatives, zeros, and positives
+
+        nums.sort()
+        ans = []
+        
+        poss = [val for val in nums if val > 0]
+        negs = [val for val in nums if val < 0]
+        zeros = [val for val in nums if val == 0]
+        
+        N, P = set(negs), set(poss)
+        
+        if zeros:
+            for val in N:
+                if -val in P:
+                    triplet = [val, 0, -val]
+                    if triplet not in ans:
+                        ans.append(triplet)
+        
+        if len(zeros) >= 3:
+            ans.append(triplet)
+            
+        
+        for i in range(len(negs) - 1):
+            for j in range(i+1, len(negs)):
+                negs_k = -(negs[i] + negs[j])
+                if negs_k in N:
+                    triplet = [negs[i], negs[j], negs_k]
+                    if triplet not in ans:
+                        ans.append(triplet)
+        
+        for i in range(len(poss) - 1):
+            for j in range(i+1, len(poss)):
+                poss_k = -(poss[i] + poss[j])
+                if poss_k in N:
+                    triplet = [poss[i], poss[j], poss_k]
+                    if triplet not in ans:
+                        ans.append(triplet)
+        return ans
+        
 
 nums = [0]    
 ans = Solution().threeSum(nums)
