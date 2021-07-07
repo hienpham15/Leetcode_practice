73312bc968d7550f3645c6b036370dd6fe80853d
hienpham15 <phamxuanhien15@gmail.com>
2021-07-07 15:50:14 +0200

    Update

--- 26_Remove_Duplicates_from_Sorted_Array.py
+++ 26_Remove_Duplicates_from_Sorted_Array.py
@@ -6,6 +6,10 @@ Created on Mon Jul  5 22:44:00 2021
 @author: hienpham
 """
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
+    def removeDuplicates(self, nums) -> int:
         nums[:] = sorted(set(nums))
         return len(nums)
+    
+    
+nums = [-1,0,0,0,0,3,3]
+ans = Solution().removeDuplicates(nums)
