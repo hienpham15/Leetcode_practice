2452458a811ba2b3a96bb3ac640e1bdd74f21f5c
hienpham15 <phamxuanhien15@gmail.com>
2021-07-09 03:17:29 +0200

    Add DFS solution

--- 39_Combination_Sum.py
+++ 39_Combination_Sum.py
@@ -9,30 +9,41 @@ Created on Fri Jul  9 00:22:48 2021
 class Solution:
     def combinationSum(self, candidates, target: int):
         ans = set()
-        path = []
         candidates = sorted(candidates)
         def dp(target, candidates, path, ans):
             for i in range(len(candidates)):
                 rem = target - candidates[i]
                 
                 if rem > 0:
-                    path += [candidates[i]]
-                    dp(rem, candidates, path, ans)
+                    dp(rem, candidates, path + [candidates[i]], ans)
                 elif rem == 0:
                     ans.add(tuple(sorted(path + [candidates[i]])))
                     return
                 else:
                     return
-                path.pop()
                 #self.path += dp(rem, d)
             
-        dp(target, candidates, path, ans)
+        dp(target, candidates, [], ans)
         return list(list(x) for x in ans)
         
+    
+    def combinationSum_v2(self, candidates, target):
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+    
+    
+    def dfs(self, nums, target, path, ret):
+        if target < 0:
+            return 
+        if target == 0:
+            ret.append(path)
+            return 
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
         
-        
-candidates = [2,7,6,3,5,1]
-target = 9
+candidates = [2, 3, 6, 7]
+target = 7
 ans = Solution().combinationSum(candidates, target)            
        
             
