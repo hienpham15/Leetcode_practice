9ee7085bc98d430a83b820299b9c097c81f2b32d
hienpham15 <phamxuanhien15@gmail.com>
2021-07-12 21:31:05 +0200

    Add 2 pointer Solution

--- 42_Trapping_Rain_Water.py
+++ 42_Trapping_Rain_Water.py
@@ -22,5 +22,32 @@ class Solution:
             ans += min(left_max, right_max) - height[i]
         return ans
     
+
+    def trap_v2(self, height):
+        left = 0
+        right = len(height) - 1
+        
+        left_max = 0
+        right_max = 0
+        
+        ans = 0
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] >= left_max:
+                    left_max = height[left]
+                else:
+                    ans += left_max - height[left]
+                    
+                left += 1
+            else:
+                if height[right] >= right_max:
+                    right_max = height[right]
+                else:
+                    ans += right_max - height[right]
+                    
+                right -= 1
+        return ans
+    
+    
 height = [4, 2, 0, 3, 2, 5]
-ans = Solution().trap(height)    
+ans = Solution().trap_v2(height)    
