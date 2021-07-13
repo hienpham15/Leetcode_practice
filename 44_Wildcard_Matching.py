582587626fede3e6c84eaea60680c6f2e406067f
hienpham15 <phamxuanhien15@gmail.com>
2021-07-13 23:33:51 +0200

    Solution using bit manipulation O(m+n)

---
+++ 44_Wildcard_Matching.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 13 23:32:25 2021
+
+@author: hienpham
+"""
+from collections import defaultdict
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        nexts = 1
+        end_bit = 1<<len(s)
+        d = defaultdict(int)
+        for i,c in enumerate(s):
+            d[c] |= 1<<i
+        for c in p:
+            if c == '*':
+                nexts =  (end_bit<<1) - (nexts & -nexts)
+            elif c == '?':
+                nexts <<= 1
+            else:
+                nexts = (nexts & d[c]) << 1
+            if nexts == 0:
+                return False
+        return nexts & end_bit != 0
