d874030ea73ace06d687e19fe72c019259467285
hienpham15 <phamxuanhien15@gmail.com>
2021-07-15 23:09:03 +0200

    Add greedy solution O(n)

--- 45_Jump_Game_II.py
+++ 45_Jump_Game_II.py
@@ -31,5 +31,25 @@ class Solution:
         self.dp(0, nums, [], d)
         return min(d)
     
-nums = [2, 1]
-ans = Solution().jump(nums)
+    
+    def jump_v2(self, nums):
+        if len(nums) <= 1:
+            return 0
+        
+        left = 0
+        right = nums[0]
+        n_jump = 1
+        
+        while right < len(nums) - 1:
+            
+            possible_jump = [i + nums[i] for i in range(left, right + 1)]
+            left = right + 1
+            right = max(possible_jump)
+            n_jump +=1
+        
+        return n_jump
+            
+            
+    
+nums = [2, 3, 1, 1, 4]
+ans = Solution().jump_v2(nums)
