24baeed350f2663873c93b5587fc1a6a30257e54
hienpham15 <phamxuanhien15@gmail.com>
2021-07-16 12:07:37 +0200

    Initial DP solution

--- 44_Wildcard_Matching.py
+++ 44_Wildcard_Matching.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Jul 13 23:32:25 2021
+Created on Fri Jul 16 10:02:31 2021
 
 @author: hienpham
 """
-from collections import defaultdict
 class Solution:
+    def dp(self, i, j, s, p):
+        if i > len(s) - 1 and j > len(p) - 1:
+            return True
+        elif i > len(s) - 1:
+            return False
+        elif j > len(p) - 1:
+            return False
+        
+        if p[j] in {s[i], "?"}:
+            ans = self.dp(i+1, j+1, s ,p)
+        elif p[j] == "*":
+            ans = self.dp(i, j+1, s, p) or self.dp(i+1, j, s, p)
+        else:
+            return False
+        return ans
+        
     def isMatch(self, s: str, p: str) -> bool:
-        nexts = 1
-        end_bit = 1<<len(s)
-        d = defaultdict(int)
-        for i,c in enumerate(s):
-            d[c] |= 1<<i
-        for c in p:
-            if c == '*':
-                nexts =  (end_bit<<1) - (nexts & -nexts)
-            elif c == '?':
-                nexts <<= 1
-            else:
-                nexts = (nexts & d[c]) << 1
-            if nexts == 0:
-                return False
-        return nexts & end_bit != 0
+        return self.dp(0, 0, s, p)
+    
+s = 'acccb'
+p = 'a*c?b'
+ans = Solution().isMatch(s, p)
+        
