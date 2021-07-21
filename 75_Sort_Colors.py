8ba95ac0d049dbcd60b67b5e84ba776ed27fd2e2
hienpham15 <phamxuanhien15@gmail.com>
2021-07-21 23:10:52 +0200

    Solution from discussion

---
+++ 75_Sort_Colors.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 21 23:09:18 2021
+
+@author: hienpham
+"""
+
+
+def sortColors(self, nums):
+    red, white, blue = 0, 0, len(nums)-1
+    
+    while white <= blue:
+        if nums[white] == 0:
+            nums[red], nums[white] = nums[white], nums[red]
+            white += 1
+            red += 1
+        elif nums[white] == 1:
+            white += 1
+        else:
+            nums[white], nums[blue] = nums[blue], nums[white]
+            blue -= 1
