71855209e3f02b491a18f180d093d7217fc5d5b5
hienpham15 <phamxuanhien15@gmail.com>
2021-07-27 23:20:18 +0200

    AC

---
+++ 84_Largest_Rectangle_in_Histogram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 27 23:19:30 2021
+
+@author: hienpham
+"""
+def largestRectangleArea(self, height):
+    height.append(0)
+    stack = [-1]
+    ans = 0
+    for i in xrange(len(height)):
+        while height[i] < height[stack[-1]]:
+            h = height[stack.pop()]
+            w = i - stack[-1] - 1
+            ans = max(ans, h * w)
+        stack.append(i)
+    height.pop()
+    return ans
