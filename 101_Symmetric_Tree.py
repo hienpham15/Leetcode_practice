817feac9364507d2b491e8a7060cb7e40ca6c115
hienpham15 <phamxuanhien15@gmail.com>
2021-08-03 23:17:10 +0200

    Stack+Recursive

---
+++ 101_Symmetric_Tree.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  3 23:16:04 2021
+
+@author: hienpham
+"""
+class Solution:
+  def isSymmetric(self, root):
+    if root is None:
+      return True
+
+    stack = [[root.left, root.right]]
+
+    while len(stack) > 0:
+      pair = stack.pop(0)
+      left = pair[0]
+      right = pair[1]
+
+      if left is None and right is None:
+        continue
+      if left is None or right is None:
+        return False
+      if left.val == right.val:
+        stack.insert(0, [left.left, right.right])
+
+        stack.insert(0, [left.right, right.left])
+      else:
+        return False
+    return True
