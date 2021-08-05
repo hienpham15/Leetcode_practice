a165899339e16137b5b539cd2732027bc62982d1
hienpham15 <phamxuanhien15@gmail.com>
2021-08-05 23:25:53 +0200

    AC

---
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 23:24:58 2021
+
+@author: hienpham
+"""
+class Solution:
+    # @param num, a list of integers
+    # @return a tree node
+    # 12:37
+    def sortedArrayToBST(self, num):
+        if not num:
+            return None
+
+        mid = len(num) // 2
+
+        root = TreeNode(num[mid])
+        root.left = self.sortedArrayToBST(num[:mid])
+        root.right = self.sortedArrayToBST(num[mid+1:])
+
+        return root
